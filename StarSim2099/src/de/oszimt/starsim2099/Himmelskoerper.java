3cbe19a928dcb28b99ba18475d77295711ec7b60
user <user@192.168.34.176>
2026-09-24 10:28:27 +0200

    Mohamed / Klasse Mond erstellt

---
+++ StarSim2099/src/de/oszimt/starsim2099/Himmelskoerper.java
@@ -0,0 +1,23 @@
+package de.oszimt.starsim2099;
+
+public class Himmelskoerper extends BigBang {
+
+	private String art;
+	private String name;
+
+	public String getArt() {
+		return art;
+	}
+
+	public void setArt(String art) {
+		this.art = art;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
